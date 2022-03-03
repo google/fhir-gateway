586e31076549f0cba1cabd9822fdc38cf207412b
Bashir Sadjad <bashir.sadjad@gmail.com>
2022-03-03 03:02:50 -0500

    Brings list-based and SoF configs to the same Keycloak instance.
    FIXES: b/214325255
    
    TESTED:
    Ran the docker compose file locally and tested the created Keycloak
    instance against the proxy. Also tested the sample Growth Chart app
    which could fetch the access token.
    
    NOTE: there is currently a weird signature validation issue with
    the SoF app which I will figure out and fix before submit and remove
    this note! But this should be ready for review.
    
    Change-Id: Iafdb252d1b29f7cec4ff2b8d8cbf70785712eee9

--- docker/keycloak/keycloak_setup.sh
+++ docker/keycloak/keycloak_setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#  Copyright 2021-2022 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
 # This is for setting up a test Keycloak server. It is intended to be run as
 # part of the docker container initialization; see Dockerfile's entrypoint.
 #
@@ -14,8 +29,13 @@
 # Environment variables used in this setup are:
 # KEYCLOAK_USER: the admin user name for Keycloak
 # KEYCLOAK_PASSWORD: the password for the admin user
-# FHIR_USER: the user to be created for accessing the FHIR store
-# FHIR_PASS: the password of the test user
+# KEYCLOAK_BASE_URL: the base URL of the Keycloak instance being configured
+# TEST_REALM: the Keycloak realm in which the user configuration is created
+# TEST_USER: the user to be created for accessing the FHIR store
+# TEST_PASS: the password of the test user
+# SMART_REALM: if set, this realm is modified by adding the above user, mapping
+#   it to a Patient resource and set up the Growth Chart SoF app as a client.
+# SMART_PATIENT_ID: the logical id of the mapped FHIR Patient resource
 
 cd /opt/jboss/keycloak/bin || exit 1
 
@@ -23,7 +43,8 @@ success="no"
 while [[ "${success}" != "yes" ]]; do
   # Login as admin first; wait until this is successful which makes sure that
   # the Keycloak server is up and running.
-  sh kcadm.sh config credentials --server http://localhost:8080/auth  \
+  sh kcadm.sh config credentials \
+    --server "${KEYCLOAK_BASE_URL:-http://localhost:9080/auth}"  \
     --realm master --user ${KEYCLOAK_USER} --password ${KEYCLOAK_PASSWORD}
   if [[ $? -eq 0 ]]; then
     success="yes"
@@ -34,44 +55,81 @@ while [[ "${success}" != "yes" ]]; do
 done
 echo "SUCCESS; logged in with admin password!"
 
-# Create a new `test` realm.
-sh kcadm.sh create realms -s realm=test -s enabled=true -s sslRequired=none
+# From this point on, fail upon any failure.
+set -e
+
+echo "Creating a new test realm; never use this for production purposes!"
+readonly REALM=${TEST_REALM:-test}
+sh kcadm.sh create realms -s realm=${REALM} -s enabled=true \
+  -s sslRequired=none
 
 # Enable saving of events.
-sh kcadm.sh update events/config -r test -s eventsEnabled=true \
+sh kcadm.sh update events/config -r ${REALM} -s eventsEnabled=true \
   -s adminEventsEnabled=true
 
 # Update the list of saved events; note although the documentation says that
 # with an empty list all events will be enabled, that is not ture. Namely, the
 # REFRESH_* events are not enabled by default; hence this enumeration!
-sh kcadm.sh update events/config -r test \
+sh kcadm.sh update events/config -r ${REALM} \
   -s 'enabledEventTypes=['"$(cat keycloak_events_list.txt)"']'
 
-# Create a new public client with direct-grant enabled.
-CID=$(sh kcadm.sh create clients -r test -s clientId=my-fhir-client \
+# Create a new public client with direct-grant enabled. This is useful for an
+# app developed by the FHIR SDK: https://github.com/google/android-fhir/
+CID=$(sh kcadm.sh create clients -r ${REALM} -s clientId=my-fhir-client \
   -s publicClient=true -s directAccessGrantsEnabled=true \
-  -s redirectUris='["com.google.android.fhir.reference:/oauth2redirect","https://www.google.com"]' \
-  -i)
-echo "Created the new client ${CID}"
+  -s redirectUris='["com.google.android.fhir.reference:/oauth2redirect"]' -i)
+echo "Created the new 'my-fhir-client' client ${CID}"
 
 # TODO remove the group setup after all proxy uses are upgraded.
 # Create a group which will be returned in `group` claim of issued tokens.
-sh kcadm.sh create groups -r test -s name=fhirUser
+sh kcadm.sh create groups -r ${REALM} -s name=fhirUser
 
 # Add the protocol-mapper for adding `group` claim.
-sh kcadm.sh create -r test clients/${CID}/protocol-mappers/models/ \
+sh kcadm.sh create -r ${REALM} clients/${CID}/protocol-mappers/models/ \
   -s name=group-fhir  -s protocolMapper=oidc-group-membership-mapper \
   -s protocol=openid-connect \
   -s config='{"full.path":"false","id.token.claim":"true","access.token.claim":"true","claim.name":"group","userinfo.token.claim":"true"}'
 
 # Create a protocol-mapper for `patient_list` user attribute.
-sh kcadm.sh create -r test clients/${CID}/protocol-mappers/models/ \
+sh kcadm.sh create -r ${REALM} clients/${CID}/protocol-mappers/models/ \
   -s name=list-mapper -s protocolMapper=oidc-usermodel-attribute-mapper \
   -s protocol=openid-connect \
   -s config='{"user.attribute":"patient_list","claim.name":"patient_list","id.token.claim":"true","access.token.claim":"true","userinfo.token.claim":"true"}'
 
 # Create the test user; set its password, group, etc.
-sh kcadm.sh create users -r test -s username=${FHIR_USER} \
+sh kcadm.sh create users -r ${REALM} -s username=${TEST_USER} \
   -s groups='["fhirUser"]' -s enabled=true \
   -s attributes='{"patient_list":"patient-list-example"}' \
-  -s credentials='[{"type":"password","value":"'${FHIR_PASS}'","temporary":false}]'
+  -s credentials='[{"type":"password","value":"'${TEST_PASS}'","temporary":false}]'
+
+echo "Setting up list-based ${REALM} realm is complete."
+
+##############################################
+# SMART config
+##############################################
+
+if [[ -z "${SMART_REALM}" ]]; then
+  echo "SMART_REALM env variable is not set; not adding SMART user/client."
+  exit 0
+fi
+
+echo "Configuring the SMART realm ${SMART_REALM}; not for production use!"
+
+# Disable SSL for easy local testing (never use in production).
+sh kcadm.sh update / -r ${SMART_REALM} -s sslRequired=none
+
+# Create a new public client for the Growth Chart SMART app. This assumes the
+# app is running locally on port 9000. Direct grant is enabled for easy testing.
+SCID=$(sh kcadm.sh create clients -r ${SMART_REALM} -s clientId=growth_chart \
+  -s publicClient=true -s directAccessGrantsEnabled=true \
+  -s redirectUris='["http://localhost:9000/"]' -i)
+echo "Created the new 'growth_chart' client ${SCID}"
+
+# Create a new user in this realm with the same user credentials as before.
+sh kcadm.sh create users -r ${SMART_REALM} -s username=${TEST_USER} \
+  -s groups='["fhirUser"]' -s enabled=true \
+  -s attributes='{"resourceId":"'${SMART_PATIENT_ID}'"}' \
+  -s credentials='[{"type":"password","value":"'${TEST_PASS}'","temporary":false}]'
+
+echo "Setting up patient-based ${SMART_REALM} realm is complete."
+
