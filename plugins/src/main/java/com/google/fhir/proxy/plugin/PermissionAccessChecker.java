3574c20720ca43ad408dccc653c8211f844758a9
Martin Ndegwa <mndegwa@ona.io>
2022-10-04 11:28:20 +0300

    Implement GET Resource Request Permission Checker ✨ - Refactor Permission checker to be Generic - Update Spotless Check dependencies

--- plugins/src/main/java/com/google/fhir/proxy/plugin/PermissionAccessChecker.java
+++ plugins/src/main/java/com/google/fhir/proxy/plugin/PermissionAccessChecker.java
@@ -16,94 +16,105 @@
 package com.google.fhir.proxy.plugin;
 
 import ca.uhn.fhir.context.FhirContext;
-
+import ca.uhn.fhir.rest.api.RequestTypeEnum;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.google.fhir.proxy.*;
+import com.google.fhir.proxy.HttpFhirClient;
 import com.google.fhir.proxy.interfaces.*;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.inject.Named;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Named;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class PermissionAccessChecker implements AccessChecker {
 
-    private static final Logger logger = LoggerFactory.getLogger(PatientAccessChecker.class);
+  private static final Logger logger = LoggerFactory.getLogger(PermissionAccessChecker.class);
 
-    private static final String PATIENT = "Patient";
-    private static final String ORGANIZATION = "Organization";
-    private static final String COMPOSITION = "Composition";
-    private final List<String> userRoles;
+  private static final String PATIENT = "Patient";
+  private static final String ORGANIZATION = "Organization";
+  private static final String COMPOSITION = "Composition";
+  private final List<String> userRoles;
 
-    private PermissionAccessChecker(List<String> userRoles) {
-        this.userRoles = userRoles;
+  private PermissionAccessChecker(List<String> userRoles) {
+    this.userRoles = userRoles;
+  }
+
+  @Override
+  public AccessDecision checkAccess(RequestDetailsReader requestDetails) {
+
+    // For a Bundle requestDetails.getResourceName() returns null
+    if (requestDetails.getRequestType() == RequestTypeEnum.POST
+        && requestDetails.getResourceName() == null) {
+      //  return processBundle(requestDetails);
+    } else {
+
+      boolean userHasRole =
+          checkIfRoleExists(getAdminRoleName(requestDetails.getResourceName()), userRoles)
+              || checkIfRoleExists(
+                  getRelevantRoleName(
+                      requestDetails.getResourceName(), requestDetails.getRequestType().name()),
+                  userRoles);
+
+      switch (requestDetails.getRequestType()) {
+        case GET:
+          return processGet(userHasRole);
+        case POST:
+          // return processPost(requestDetails);
+        case PUT:
+          // return processPut(requestDetails);
+        case DELETE:
+          // return processDelete(requestDetails);
+        default:
+          // TODO handle other cases like PATCH
+          return NoOpAccessDecision.accessDenied();
+      }
+    }
+
+    return NoOpAccessDecision.accessDenied();
+  }
+
+  private AccessDecision processGet(boolean userHasRole) {
+    return userHasRole ? NoOpAccessDecision.accessGranted() : NoOpAccessDecision.accessDenied();
+  }
+
+  private String getRelevantRoleName(String resourceName, String methodType) {
+    return methodType + "_" + resourceName.toUpperCase();
+  }
+
+  private String getAdminRoleName(String resourceName) {
+    return "MANAGE_" + resourceName.toUpperCase();
+  }
+
+  private boolean checkIfRoleExists(String roleName, List<String> existingRoles) {
+    return existingRoles.contains(roleName);
+  }
+
+  @Named(value = "permission")
+  static class Factory implements AccessCheckerFactory {
+
+    private List<String> getUserRolesFromJWT(DecodedJWT jwt) {
+      Claim claim = jwt.getClaim("realm_access");
+      Map<String, Object> roles = claim.asMap();
+      List<Object> collection = roles.values().stream().collect(Collectors.toList());
+      List<String> rolesList = new ArrayList<>();
+      List<Object> collectionPart = (List<Object>) collection.get(0);
+      for (Object collect : collectionPart) {
+        rolesList.add(collect.toString());
+      }
+      return rolesList;
     }
 
     @Override
-    public AccessDecision checkAccess(RequestDetailsReader requestDetails) {
-
-        switch (requestDetails.getRequestType()) {
-        case GET:
-            if ((requestDetails.getResourceName()).equals(PATIENT) || (requestDetails.getResourceName()).equals(ORGANIZATION) || (requestDetails.getResourceName()).equals(ORGANIZATION)) {
-                boolean result = checkIfRoleExists(getRelevantRoleName(requestDetails.getResourceName(), requestDetails.getRequestType()
-                  .name()), userRoles);
-                if (result) {
-                    logger.info("Access Granted");
-                    return NoOpAccessDecision.accessGranted();
-                } else {
-                    logger.info("Access Denied");
-                    return NoOpAccessDecision.accessDenied();
-                }
-            }
-        case POST:
-
-        case PUT:
-
-        case PATCH:
-
-        default:
-            // TODO handle other cases like DELETE
-            return NoOpAccessDecision.accessDenied();
-        }
+    public AccessChecker create(
+        DecodedJWT jwt,
+        HttpFhirClient httpFhirClient,
+        FhirContext fhirContext,
+        PatientFinder patientFinder) {
+      List<String> userRoles = getUserRolesFromJWT(jwt);
+      return new PermissionAccessChecker(userRoles);
     }
-
-    private String getRelevantRoleName(String resourceName, String methodType) {
-        return methodType + "_" + resourceName.toUpperCase();
-    }
-
-    private boolean checkIfRoleExists(String roleName, List<String> existingRoles) {
-        if (existingRoles.contains(roleName)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Named(value = "permission")
-    static class Factory implements AccessCheckerFactory {
-
-        private List<String> getUserRolesFromJWT(DecodedJWT jwt) {
-            Claim claim = jwt.getClaim("realm_access");
-            Map<String, Object> roles = claim.asMap();
-            List<Object> collection = roles.values()
-              .stream()
-              .collect(Collectors.toList());
-            List<String> rolesList = new ArrayList<>();
-            List<Object> collectionPart = (List<Object>) collection.get(0);
-            for (Object collect : collectionPart) {
-                rolesList.add(collect.toString());
-            }
-            return rolesList;
-        }
-
-        @Override
-        public AccessChecker create(DecodedJWT jwt, HttpFhirClient httpFhirClient, FhirContext fhirContext, PatientFinder patientFinder) {
-            List<String> userRoles = getUserRolesFromJWT(jwt);
-            return new PermissionAccessChecker(userRoles);
-        }
-    }
+  }
 }
