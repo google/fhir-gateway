ce20adac546f1a16e0734b252e42622292fe1e37
Bashir Sadjad <bashir.sadjad@gmail.com>
2022-12-11 03:54:18 +0000

    revert spring-boot version to be Java 11 compatible and logging fixes

--- e2e-test/e2e.sh
+++ e2e-test/e2e.sh
@@ -24,8 +24,19 @@ function setup() {
   docker build -t us-docker.pkg.dev/fhir-proxy-build/stable/fhir-access-proxy:${BUILD_ID} .
   docker-compose -f docker/keycloak/config-compose.yaml \
                  up --force-recreate --remove-orphans -d --quiet-pull
+  # TODO find a way to expose docker container logs in the output; currently
+  #   with -d we don't get any logs and it makes debugging failures difficult.
+  # TODO what does `--wait` do? It does not seem to be a documented option and
+  #   fails some versions of docker-compose.
   docker-compose -f docker/hapi-proxy-compose.yaml \
-                 up --force-recreate --remove-orphans -d --quiet-pull --wait
+                 up --force-recreate --remove-orphans -d --quiet-pull
+  # Making sure that all services are up
+  # TODO this is a hack; we need to make the downstream clients more resilient.
+  echo "Containers status before wait:"
+  docker ps
+  sleep 120
+  echo "Containers status after wait:"
+  docker ps
 }
 
 setup
