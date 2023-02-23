88e119c825fd7ffcfc40f0eaedd5da67be34a966
anchita-g <109063673+anchita-g@users.noreply.github.com>
2023-02-23 10:47:37 +0530

    Supporting SMART on FHIR patient scopes in PatientAccessChecker (#89)
    * Supporting SMART on FHIR patient scopes in PatientAccessChecker

--- plugins/src/main/java/com/google/fhir/gateway/plugin/PatientAccessChecker.java
+++ plugins/src/main/java/com/google/fhir/gateway/plugin/PatientAccessChecker.java
@@ -16,6 +16,7 @@
 package com.google.fhir.gateway.plugin;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.api.RequestTypeEnum;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.common.annotations.VisibleForTesting;
@@ -32,16 +33,29 @@ import com.google.fhir.gateway.interfaces.AccessDecision;
 import com.google.fhir.gateway.interfaces.NoOpAccessDecision;
 import com.google.fhir.gateway.interfaces.PatientFinder;
 import com.google.fhir.gateway.interfaces.RequestDetailsReader;
+import com.google.fhir.gateway.plugin.SmartFhirScope.Permission;
+import com.google.fhir.gateway.plugin.SmartFhirScope.Principal;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Set;
 import javax.inject.Named;
-import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryRequestComponent;
+import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.ResourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This access-checker uses the `patient_id` claim in the access token to decide whether access to a
- * request should be granted or not.
+ * This access-checker uses the `patient_id` and `scope` claims in the access token to decide
+ * whether access to a request should be granted or not. The `scope` claims are expected to tbe
+ * SMART-on-FHIR compliant.
  */
 public class PatientAccessChecker implements AccessChecker {
   private static final Logger logger = LoggerFactory.getLogger(PatientAccessChecker.class);
@@ -49,16 +63,27 @@ public class PatientAccessChecker implements AccessChecker {
   private final String authorizedPatientId;
   private final PatientFinder patientFinder;
 
-  private PatientAccessChecker(String authorizedPatientId, PatientFinder patientFinder) {
+  private final FhirContext fhirContext;
+
+  private final SmartScopeChecker smartScopeChecker;
+
+  private PatientAccessChecker(
+      FhirContext fhirContext,
+      String authorizedPatientId,
+      PatientFinder patientFinder,
+      SmartScopeChecker smartScopeChecker) {
     Preconditions.checkNotNull(authorizedPatientId);
     Preconditions.checkNotNull(patientFinder);
+    Preconditions.checkNotNull(smartScopeChecker);
+    Preconditions.checkNotNull(fhirContext);
     this.authorizedPatientId = authorizedPatientId;
     this.patientFinder = patientFinder;
+    this.fhirContext = fhirContext;
+    this.smartScopeChecker = smartScopeChecker;
   }
 
   @Override
   public AccessDecision checkAccess(RequestDetailsReader requestDetails) {
-
     // For a Bundle requestDetails.getResourceName() returns null
     if (requestDetails.getRequestType() == RequestTypeEnum.POST
         && requestDetails.getResourceName() == null) {
@@ -71,9 +96,10 @@ public class PatientAccessChecker implements AccessChecker {
       case POST:
         return processPost(requestDetails);
       case PUT:
-        return processPut(requestDetails);
+        // TODO(https://github.com/google/fhir-gateway/issues/88): Support update as create
+        // operation
       case PATCH:
-        return processPatch(requestDetails);
+        return processUpdate(requestDetails);
       case DELETE:
         return processDelete(requestDetails);
       default:
@@ -82,31 +108,53 @@ public class PatientAccessChecker implements AccessChecker {
   }
 
   private AccessDecision processGet(RequestDetailsReader requestDetails) {
-    String patientId = patientFinder.findPatientFromParams(requestDetails);
-    return new NoOpAccessDecision(authorizedPatientId.equals(patientId));
+    if (requestDetails.getResourceName() == null) {
+      return NoOpAccessDecision.accessDenied();
+    }
+    // This operation corresponds to the read/vread/history operations on instance
+    if (requestDetails.getId() != null) {
+      return processRead(requestDetails);
+    }
+    return processSearch(requestDetails);
   }
 
   private AccessDecision processPost(RequestDetailsReader requestDetails) {
+    // TODO(https://github.com/google/fhir-gateway/issues/87): Add support for search in post
+    return processCreate(requestDetails);
+  }
+
+  private AccessDecision processRead(RequestDetailsReader requestDetails) {
+    String patientId = patientFinder.findPatientFromParams(requestDetails);
+    return new NoOpAccessDecision(
+        authorizedPatientId.equals(patientId)
+            && smartScopeChecker.hasPermission(requestDetails.getResourceName(), Permission.READ));
+  }
+
+  private AccessDecision processSearch(RequestDetailsReader requestDetails) {
+    String patientId = patientFinder.findPatientFromParams(requestDetails);
+    return new NoOpAccessDecision(
+        authorizedPatientId.equals(patientId)
+            && smartScopeChecker.hasPermission(
+                requestDetails.getResourceName(), Permission.SEARCH));
+  }
+
+  private AccessDecision processCreate(RequestDetailsReader requestDetails) {
     // This AccessChecker does not accept new patients.
     if (FhirUtil.isSameResourceType(requestDetails.getResourceName(), ResourceType.Patient)) {
       return NoOpAccessDecision.accessDenied();
     }
     Set<String> patientIds = patientFinder.findPatientsInResource(requestDetails);
-    return new NoOpAccessDecision(patientIds.contains(authorizedPatientId));
+    return new NoOpAccessDecision(
+        patientIds.contains(authorizedPatientId)
+            && smartScopeChecker.hasPermission(
+                requestDetails.getResourceName(), Permission.CREATE));
   }
 
-  private AccessDecision processPut(RequestDetailsReader requestDetails) {
+  private AccessDecision processUpdate(RequestDetailsReader requestDetails) {
     if (FhirUtil.isSameResourceType(requestDetails.getResourceName(), ResourceType.Patient)) {
       return checkPatientAccessInUpdate(requestDetails);
     }
-    return checkNonPatientAccessInUpdate(requestDetails, HTTPVerb.PUT);
-  }
-
-  private AccessDecision processPatch(RequestDetailsReader requestDetails) {
-    if (FhirUtil.isSameResourceType(requestDetails.getResourceName(), ResourceType.Patient)) {
-      return checkPatientAccessInUpdate(requestDetails);
-    }
-    return checkNonPatientAccessInUpdate(requestDetails, HTTPVerb.PATCH);
+    return checkNonPatientAccessInUpdate(requestDetails, requestDetails.getRequestType());
   }
 
   private AccessDecision processDelete(RequestDetailsReader requestDetails) {
@@ -116,11 +164,14 @@ public class PatientAccessChecker implements AccessChecker {
     }
     // TODO(https://github.com/google/fhir-access-proxy/issues/63):Support direct resource deletion.
     String patientId = patientFinder.findPatientFromParams(requestDetails);
-    return new NoOpAccessDecision(authorizedPatientId.equals(patientId));
+    return new NoOpAccessDecision(
+        authorizedPatientId.equals(patientId)
+            && smartScopeChecker.hasPermission(
+                requestDetails.getResourceName(), Permission.DELETE));
   }
 
   private AccessDecision checkNonPatientAccessInUpdate(
-      RequestDetailsReader requestDetails, HTTPVerb updateMethod) {
+      RequestDetailsReader requestDetails, RequestTypeEnum updateMethod) {
     // We do not allow direct resource PUT/PATCH, so Patient ID must be returned
     String patientId = patientFinder.findPatientFromParams(requestDetails);
     if (!patientId.equals(authorizedPatientId)) {
@@ -128,17 +179,20 @@ public class PatientAccessChecker implements AccessChecker {
     }
 
     Set<String> patientIds = Sets.newHashSet();
-    if (updateMethod == HTTPVerb.PATCH) {
+    if (updateMethod == RequestTypeEnum.PATCH) {
       patientIds =
           patientFinder.findPatientsInPatch(requestDetails, requestDetails.getResourceName());
       if (patientIds.isEmpty()) {
         return NoOpAccessDecision.accessGranted();
       }
     }
-    if (updateMethod == HTTPVerb.PUT) {
+    if (updateMethod == RequestTypeEnum.PUT) {
       patientIds = patientFinder.findPatientsInResource(requestDetails);
     }
-    return new NoOpAccessDecision(patientIds.contains(authorizedPatientId));
+    return new NoOpAccessDecision(
+        patientIds.contains(authorizedPatientId)
+            && smartScopeChecker.hasPermission(
+                requestDetails.getResourceName(), Permission.UPDATE));
   }
 
   private AccessDecision checkPatientAccessInUpdate(RequestDetailsReader requestDetails) {
@@ -148,7 +202,9 @@ public class PatientAccessChecker implements AccessChecker {
       logger.error("The provided Patient resource has no ID; denying access!");
       return NoOpAccessDecision.accessDenied();
     }
-    return new NoOpAccessDecision(authorizedPatientId.equals(patientId));
+    return new NoOpAccessDecision(
+        authorizedPatientId.equals(patientId)
+            && smartScopeChecker.hasPermission(ResourceType.Patient.name(), Permission.UPDATE));
   }
 
   private AccessDecision processBundle(RequestDetailsReader requestDetails) {
@@ -170,7 +226,91 @@ public class PatientAccessChecker implements AccessChecker {
         return NoOpAccessDecision.accessDenied();
       }
     }
+    // TODO: there is duplication in processing the request Bundle here. It is already processed in
+    // PatientFinder
+    // Will follow up with a PR to solve this :https://github.com/google/fhir-gateway/issues/104
+    Bundle requestBundle = createBundleFromRequest(requestDetails);
+    if (requestBundle == null) {
+      return NoOpAccessDecision.accessDenied();
+    }
+    for (BundleEntryComponent entryComponent : requestBundle.getEntry()) {
+      if (!doesBundleElementHavePermission(entryComponent)) {
+        return NoOpAccessDecision.accessDenied();
+      }
+    }
     return NoOpAccessDecision.accessGranted();
+  }
+
+  private Bundle createBundleFromRequest(RequestDetailsReader request) {
+    byte[] requestContentBytes = request.loadRequestContents();
+    Charset charset = request.getCharset();
+    if (charset == null) {
+      charset = StandardCharsets.UTF_8;
+    }
+    String requestContent = new String(requestContentBytes, charset);
+    IParser jsonParser = fhirContext.newJsonParser();
+    IBaseResource resource = jsonParser.parseResource(requestContent);
+    if (!(resource instanceof Bundle)) {
+      return null;
+    }
+    return (Bundle) resource;
+  }
+
+  private boolean doesReferenceElementHavePermission(
+      IIdType referenceElement, Permission permission) {
+    if (referenceElement.getResourceType() != null && referenceElement.hasIdPart()) {
+      return smartScopeChecker.hasPermission(referenceElement.getResourceType(), permission);
+    } else {
+      return smartScopeChecker.hasPermission(referenceElement.getValue(), permission);
+    }
+  }
+
+  private boolean doesBundleElementHavePermission(BundleEntryComponent bundleEntry) {
+    BundleEntryRequestComponent bundleEntryRequest = bundleEntry.getRequest();
+    try {
+      switch (bundleEntryRequest.getMethod()) {
+        case GET:
+          if (bundleEntryRequest.getUrl() != null) {
+            URI resourceUri = new URI(bundleEntryRequest.getUrl());
+            IIdType referenceElement = new Reference(resourceUri.getPath()).getReferenceElement();
+            return doesReferenceElementHavePermission(referenceElement, Permission.READ);
+          }
+          break;
+        case POST:
+          if (bundleEntry.getResource().getResourceType() != null) {
+            return smartScopeChecker.hasPermission(
+                bundleEntry.getResource().getResourceType().name(), Permission.CREATE);
+          }
+          // TODO(https://github.com/google/fhir-gateway/issues/87): Add support for search in post
+          break;
+        case PUT:
+          if (bundleEntryRequest.getUrl() != null) {
+            URI resourceUri = new URI(bundleEntryRequest.getUrl());
+            IIdType referenceElement = new Reference(resourceUri.getPath()).getReferenceElement();
+            return doesReferenceElementHavePermission(referenceElement, Permission.UPDATE);
+          }
+          break;
+        case PATCH:
+          if (bundleEntryRequest.getUrl() != null) {
+            URI resourceUri = new URI(bundleEntryRequest.getUrl());
+            IIdType referenceElement = new Reference(resourceUri.getPath()).getReferenceElement();
+            return doesReferenceElementHavePermission(referenceElement, Permission.UPDATE);
+          }
+        case DELETE:
+          if (bundleEntryRequest.getUrl() != null) {
+            URI resourceUri = new URI(bundleEntryRequest.getUrl());
+            IIdType referenceElement = new Reference(resourceUri.getPath()).getReferenceElement();
+            return doesReferenceElementHavePermission(referenceElement, Permission.DELETE);
+          }
+          break;
+        default:
+          return false;
+      }
+    } catch (URISyntaxException e) {
+      logger.error(
+          String.format("Error in parsing bundle request url %s", bundleEntryRequest.getUrl()));
+    }
+    return false;
   }
 
   @Named(value = "patient")
@@ -178,8 +318,18 @@ public class PatientAccessChecker implements AccessChecker {
 
     @VisibleForTesting static final String PATIENT_CLAIM = "patient_id";
 
+    @VisibleForTesting static final String SCOPES_CLAIM = "scope";
+
     private String getPatientId(DecodedJWT jwt) {
       return FhirUtil.checkIdOrFail(JwtUtil.getClaimOrDie(jwt, PATIENT_CLAIM));
+    }
+
+    private SmartScopeChecker getSmartFhirPermissionChecker(DecodedJWT jwt) {
+      String scopesClaim = JwtUtil.getClaimOrDie(jwt, SCOPES_CLAIM);
+      String[] scopes = scopesClaim.strip().split("\\s+");
+      return new SmartScopeChecker(
+          SmartFhirScope.extractSmartFhirScopesFromTokens(Arrays.asList(scopes)),
+          Principal.PATIENT);
     }
 
     public AccessChecker create(
@@ -187,7 +337,8 @@ public class PatientAccessChecker implements AccessChecker {
         HttpFhirClient httpFhirClient,
         FhirContext fhirContext,
         PatientFinder patientFinder) {
-      return new PatientAccessChecker(getPatientId(jwt), patientFinder);
+      return new PatientAccessChecker(
+          fhirContext, getPatientId(jwt), patientFinder, getSmartFhirPermissionChecker(jwt));
     }
   }
 }
---
+++ plugins/src/main/java/com/google/fhir/gateway/plugin/SmartFhirScope.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2021-2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.fhir.gateway.plugin;
+
+import com.google.common.base.Preconditions;
+import com.google.fhir.gateway.FhirUtil;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import lombok.Getter;
+
+/**
+ * This class models the SMART-on-FHIR permission scopes that are meant ot be used for accessing
+ * clinical data. The constraints in this class are according to the official guidelines here:
+ * https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html#scopes-for-requesting-clinical-data
+ */
+@Getter
+public class SmartFhirScope {
+  private static final Pattern VALID_SCOPE_PATTERN =
+      Pattern.compile(
+          "(\\buser|patient|system\\b)(\\/((\\*)|([a-zA-Z]+)))(\\.((\\*)|([cruds]+)|(\\bread|write\\b)))");
+  static final String ALL_RESOURCE_TYPES_WILDCARD = "*";
+  private static final String ALL_RESOURCE_PERMISSIONS_WILDCARD = "*";
+  private static final String SMART_V1_READ_RESOURCE_PERMISSIONS = "read";
+  private static final String SMART_V1_WRITE_RESOURCE_PERMISSIONS = "write";
+
+  private final Principal principal;
+  private final String resourceType;
+  private final Set<Permission> permissions;
+
+  private SmartFhirScope(
+      Principal principal, String resourceType, Set<Permission> resourcePermissions) {
+    this.principal = principal;
+    this.permissions = resourcePermissions;
+    this.resourceType = resourceType;
+  }
+
+  static List<SmartFhirScope> extractSmartFhirScopesFromTokens(List<String> tokens) {
+    List<SmartFhirScope> scopes = new ArrayList<>();
+    for (String scope : tokens) {
+      if (VALID_SCOPE_PATTERN.matcher(scope).matches()) {
+        scopes.add(createSmartScope(scope));
+      }
+    }
+    return scopes;
+  }
+
+  private static SmartFhirScope createSmartScope(String scope) {
+    String[] split = scope.split("/");
+    Preconditions.checkArgument(split.length == 2);
+    Principal principal = Principal.getPrincipal(split[0]);
+    String[] permissionSplit = split[1].split("\\.");
+    Preconditions.checkArgument(permissionSplit.length == 2);
+    if (!isValidResourceType(permissionSplit[0])) {
+      throw new IllegalArgumentException(
+          String.format("Invalid resource type %s", permissionSplit[0]));
+    }
+    String resourceType = permissionSplit[0];
+    Set<Permission> permissions = extractPermissions(permissionSplit[1]);
+    return new SmartFhirScope(principal, resourceType, permissions);
+  }
+
+  private static boolean isValidResourceType(String resourceType) {
+    return ALL_RESOURCE_TYPES_WILDCARD.equals(resourceType)
+        || FhirUtil.isValidFhirResourceType(resourceType);
+  }
+
+  private static Set<Permission> extractPermissions(String permissionString) {
+    Set<Permission> permissions = new HashSet<>();
+    if (ALL_RESOURCE_PERMISSIONS_WILDCARD.equals(permissionString)) {
+      permissions.addAll(List.of(Permission.values()));
+      return permissions;
+    }
+    // We will support both v1 and v2 versions of the permissions:
+    // https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html#scopes-for-requesting-clinical-data
+    if (SMART_V1_READ_RESOURCE_PERMISSIONS.equals(permissionString)) {
+      permissions.add(Permission.READ);
+      permissions.add(Permission.SEARCH);
+      return permissions;
+    }
+    if (SMART_V1_WRITE_RESOURCE_PERMISSIONS.equals(permissionString)) {
+      permissions.add(Permission.CREATE);
+      permissions.add(Permission.UPDATE);
+      permissions.add(Permission.DELETE);
+      return permissions;
+    }
+    char[] permissionTokens = permissionString.toCharArray();
+    int permissionTokensCounter = 0;
+    // SMART guidelines recommend enforcing order in the permissions string:
+    // https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html#scopes-for-requesting-clinical-data
+    for (Permission permission : Permission.values()) {
+      if (Permission.getPermission(permissionTokens[permissionTokensCounter]) == permission) {
+        permissionTokensCounter++;
+        permissions.add(permission);
+      }
+      if (permissionTokensCounter == permissionTokens.length) {
+        break;
+      }
+    }
+    if (permissionTokensCounter != permissionTokens.length) {
+      throw new IllegalArgumentException(
+          String.format("Invalid permission string %s", permissionString));
+    }
+    return permissions;
+  }
+
+  enum Principal {
+    USER,
+    PATIENT,
+    SYSTEM;
+
+    static Principal getPrincipal(String principal) {
+      return Principal.valueOf(principal.toUpperCase());
+    }
+  }
+
+  /**
+   * SMART Permission to specify the kind of permission that is allowed on a Resource. The order of
+   * the Permission is important to us in the way it has been listed here. Please see:
+   * https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html#scopes-for-requesting-clinical-data
+   * The given order of Permissions is how it is expected that permissions will be specified in the
+   * token claim
+   */
+  enum Permission {
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+    SEARCH;
+
+    static Permission getPermission(char permissionCode) {
+      switch (permissionCode) {
+        case 'c':
+          return CREATE;
+        case 'r':
+          return READ;
+        case 'u':
+          return UPDATE;
+        case 'd':
+          return DELETE;
+        case 's':
+          return SEARCH;
+        default:
+          throw new IllegalArgumentException(
+              String.format("Invalid permission code. %s", permissionCode));
+      }
+    }
+  }
+}
