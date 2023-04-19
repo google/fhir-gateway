45d08bbb5d36ebc69f1cb64576b9a6a8bd76844f
vivekmittal07 <105198504+vivekmittal07@users.noreply.github.com>
2023-04-19 10:37:35 +0530

    Updated the scan paths for demo app to load the core beans (#132)
    Add default scan paths in the exec application

--- exec/src/main/java/com/google/fhir/gateway/MainApp.java
+++ exec/src/main/java/com/google/fhir/gateway/MainApp.java
@@ -19,8 +19,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
-@ServletComponentScan
+@SpringBootApplication(scanBasePackages = {"com.google.fhir.gateway.plugin"})
+@ServletComponentScan(basePackages = "com.google.fhir.gateway")
 public class MainApp {
 
   public static void main(String[] args) {
