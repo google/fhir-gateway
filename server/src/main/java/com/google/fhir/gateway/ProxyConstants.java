d5f0ffebe111c7a4e04537d838b1a9868813ea81
Reham Muzzamil <reham.muzzamil@venturedive.com>
2023-09-13 15:55:36 +0500

    Remove redundant code

--- server/src/main/java/com/google/fhir/gateway/ProxyConstants.java
+++ server/src/main/java/com/google/fhir/gateway/ProxyConstants.java
@@ -19,27 +19,6 @@ import ca.uhn.fhir.rest.api.Constants;
 import org.apache.http.entity.ContentType;
 
 public class ProxyConstants {
-
-  public static final String CARE_TEAM_TAG_URL = "https://smartregister.org/care-team-tag-id";
-
-  public static final String LOCATION_TAG_URL = "https://smartregister.org/location-tag-id";
-
-  public static final String ORGANISATION_TAG_URL = "https://smartregister.org/organisation-tag-id";
-
-  public static final String TAG_SEARCH_PARAM = "_tag";
-
-  public static final String PARAM_VALUES_SEPARATOR = ",";
-
-  public static final String CODE_URL_VALUE_SEPARATOR = "|";
-
-  public static final String HTTP_URL_SEPARATOR = "/";
-
   // Note we should not set charset here; otherwise GCP FHIR store complains about Content-Type.
   static final ContentType JSON_PATCH_CONTENT = ContentType.create(Constants.CT_JSON_PATCH);
-  public static final String SYNC_STRATEGY = "syncStrategy";
-  public static final String REALM_ACCESS = "realm_access";
-
-  public interface Literals {
-    String EQUALS = "=";
-  }
 }
